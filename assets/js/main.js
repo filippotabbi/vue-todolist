b6d55d42d506116eb3367cd118a7f347eaf5289f
Filippo <filippotabbi23@gmail.com>
2021-03-25 18:13:47 +0100

    esercizio terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,56 @@
 var app = new Vue({
   el: '#app',
   data: {
+    index: 0,
     inputTxt: '',
     event: [
-      {title: 'dare da mangiare ai pesci'}
+      {title: 'dare da mangiare ai pesci',
+      status: 'todo'}
       ,
-      {title: 'lavare piatti'}
+      {title: 'lavare piatti',
+      status: 'todo'}
       ,
-      {title: 'mettere patate a bollire'}
+      {title: 'mettere patate a bollire',
+      status: 'todo'}
       ,
-      {title: 'comprare un etto di prosciutto'}
-      ,
-      {title: 'scolare le patate'}
+      {title: 'comprare un etto di prosciutto',
+      status: 'todo'}
       ,
     ],
   },
+  computed: {
+    eventcomputed: function () {
+      let eventDone = this.event.filter((todo) => todo.status == 'done');
+      let eventTodo = this.event.filter((todo) => todo.status == 'todo');
+
+      return [...eventTodo, ...eventDone];
+     }
+  },
   methods: {
-    add : function () {
+    aggiungi: function() {
       if (this.inputTxt != ''){
         let obj = {
-          title : this.inputTxt
-
+          title : this.inputTxt,
+          status : 'todo'
         }
         this.event.push(obj);
-        console.log(inputTxt);
+        this.inputTxt = '';
       }
-    }
+
+    },
+    check: function (todo) {
+    let index = this.event.indexOf(todo);
+     this.event[todo].status = 'done';
+     console.log( this.event[todo].status);
+   },
+   edit: function (index) {
+    console.log( this.event[index].status);
+  },
+  remove: function (todo) {
+    let index = this.event.indexOf(todo);
+    this.event[todo].title = '';
+
 
   }
+ }
 })
